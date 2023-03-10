c9ca6503ea0f09c5c6b3222cf2af230397657507
jan-wijman <jan.wijman@gmail.com>
2023-03-10 17:04:02 +0100

    Working WiFI for the ESP32. Changes in the init method of the wifi listener

--- MyLinxESP32/lib/LinxESP32/examples/ESP32_BluetoothSerial/ESP32_BluetoothSerial.ino
+++ MyLinxESP32/lib/LinxESP32/examples/ESP32_BluetoothSerial/ESP32_BluetoothSerial.ino
@@ -10,20 +10,19 @@
 ****************************************************************************************/
 
 //Include Device Specific Header From Sketch>>Import Library (In This Case LinxESP32.h)
-//Also Include Desired LINX Listener From Sketch>>Import Library (In This Case LinxESP32WifiListener.h)
+//Also Include Desired LINX Listener From Sketch>>Import Library (In This Case LinxSerialListener.h)
 #include <LinxESP32.h>
-#include <Arduino.h>
+
 //Create A Pointer To The LINX Device Object We Instantiate In Setup()
-LinxESP32* pLinxDevice;
+LinxESP32* LinxDevice;
 
 //Initialize LINX Device And Listener
 void setup()
 {
   //Instantiate The LINX Device
-  pLinxDevice = new LinxESP32();
-  pLinxDevice->EnableDebug(DEBUG_ENABLED);
-  
-  //PWM & Servo Setup
+  LinxDevice = new LinxESP32();
+
+  // PWM & Servo Setup
   uint8_t pwmList[] = {};         // Max16Ch {0, 26, ...}
   uint16_t pwmFrequency = 12000;  // LED:12000, Servo:50
   for (int i = 0; i < sizeof(pwmList); i++) {
@@ -36,28 +35,19 @@ void setup()
   //Wire.begin();
   //Wire1.begin(32, 33);
 
-  // The LINX Listener Is Pre Instantiated.
-  // Set SSID (Network Name), Security Type, Passphrase/Key, And Call Start With Desired Device IP and Port
-  // If not set, it will connect with the last connection information
-  LinxWifiConnection.SetSsid("VI Technologies");
-  LinxWifiConnection.SetSecurity(WPA2_PASSPHRASE);  //NONE, WPA2_PASSPHRASE, WPA2_KEY, WEP40, WEO104
-  LinxWifiConnection.SetPassphrase("3942074321882926");
-
-  //LinxWifiConnection.SetSsid("PandaGrote");
-  //LinxWifiConnection.SetSecurity(WPA2_PASSPHRASE);  //NONE, WPA2_PASSPHRASE, WPA2_KEY, WEP40, WEO104
-  //LinxWifiConnection.SetPassphrase("Zbroggbvgw-1");
-
-  //Start With Fixed Port.  When Using This Method You Cannot Update The Port Using LINX VIs
-  LinxWifiConnection.Start(pLinxDevice, 44300);
-  delay(1000);
-  pLinxDevice->EnableDebug(DEBUG_ENABLED);
+  // The LINXT Listener Is Pre Instantiated, Call Start And Pass A Pointer To The LINX Device And The UART Channel To Listen On
+  // ch0:Serial
+  // ch1:Serial1
+  // ch2:Serial2
+  // ch3:SerialBT
+  LinxSerialConnection.Start(LinxDevice, 3);
 }
 
 void loop()
 {
   //Listen For New Packets From LabVIEW
-  LinxWifiConnection.CheckForCommands();
+  LinxSerialConnection.CheckForCommands();
 
   //Your Code Here, But It will Slow Down The Connection With LabVIEW
-  
+  delay(1);
 }
--- MyLinxESP32/lib/LinxESP32/examples/ESP32_Wifi/ESP32_Wifi.ino
+++ MyLinxESP32/lib/LinxESP32/examples/ESP32_Wifi/ESP32_Wifi.ino
@@ -12,18 +12,17 @@
 //Include Device Specific Header From Sketch>>Import Library (In This Case LinxESP32.h)
 //Also Include Desired LINX Listener From Sketch>>Import Library (In This Case LinxESP32WifiListener.h)
 #include <LinxESP32.h>
-#include <Arduino.h>
+
 //Create A Pointer To The LINX Device Object We Instantiate In Setup()
-LinxESP32* pLinxDevice;
+LinxESP32* LinxDevice;
 
 //Initialize LINX Device And Listener
 void setup()
 {
   //Instantiate The LINX Device
-  pLinxDevice = new LinxESP32();
-  pLinxDevice->EnableDebug(DEBUG_ENABLED);
-  
-  //PWM & Servo Setup
+  LinxDevice = new LinxESP32();
+
+  // PWM & Servo Setup
   uint8_t pwmList[] = {};         // Max16Ch {0, 26, ...}
   uint16_t pwmFrequency = 12000;  // LED:12000, Servo:50
   for (int i = 0; i < sizeof(pwmList); i++) {
@@ -39,18 +38,12 @@ void setup()
   // The LINX Listener Is Pre Instantiated.
   // Set SSID (Network Name), Security Type, Passphrase/Key, And Call Start With Desired Device IP and Port
   // If not set, it will connect with the last connection information
-  LinxWifiConnection.SetSsid("VI Technologies");
-  LinxWifiConnection.SetSecurity(WPA2_PASSPHRASE);  //NONE, WPA2_PASSPHRASE, WPA2_KEY, WEP40, WEO104
-  LinxWifiConnection.SetPassphrase("3942074321882926");
-
-  //LinxWifiConnection.SetSsid("PandaGrote");
-  //LinxWifiConnection.SetSecurity(WPA2_PASSPHRASE);  //NONE, WPA2_PASSPHRASE, WPA2_KEY, WEP40, WEO104
-  //LinxWifiConnection.SetPassphrase("Zbroggbvgw-1");
+  //LinxWifiConnection.SetSsid("YOUR_NETWORK_NAME");
+  //LinxWifiConnection.SetSecurity(WPA2_NONE);  //NONE, WPA2_PASSPHRASE, WPA2_KEY, WEP40, WEO104
+  //LinxWifiConnection.SetPassphrase("PASSPHRASE");
 
   //Start With Fixed Port.  When Using This Method You Cannot Update The Port Using LINX VIs
-  LinxWifiConnection.Start(pLinxDevice, 44300);
-  delay(1000);
-  pLinxDevice->EnableDebug(DEBUG_ENABLED);
+  LinxWifiConnection.Start(LinxDevice, 44300);
 }
 
 void loop()
@@ -59,5 +52,5 @@ void loop()
   LinxWifiConnection.CheckForCommands();
 
   //Your Code Here, But It will Slow Down The Connection With LabVIEW
-  
+  delay(1);
 }
